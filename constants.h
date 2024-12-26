b8c5af81d8af526b92260a90f6281fb93698420e
Madalena-bdd <madalena.bordadagua@tecnico.ulisboa.pt>
2024-12-26 19:23:45 +0000

    Implementação api.c, api.h e adionar constantes necessárias.

--- constants.h
+++ constants.h
@@ -1,3 +1,10 @@
 #define MAX_WRITE_SIZE 256
 #define MAX_STRING_SIZE 40
 #define MAX_JOB_FILE_NAME_SIZE 256
+
+// constantes partilhadas entre cliente e servidor
+#define MAX_SESSION_COUNT 1  // num max de sessoes no server, 1 default, necessario alterar mais tarde
+#define STATE_ACCESS_DELAY_US  // delay a aplicar no server
+#define MAX_PIPE_PATH_LENGTH 40 // tamanho max do caminho do pipe
+#define MAX_STRING_SIZE 40
+#define MAX_NUMBER_SUB 10
